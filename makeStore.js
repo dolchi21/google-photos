3d5fd78a8978b0de8f29fa9deb552f2869be28be
Axel Dolce <axel.dolce@gmail.com>
2018-09-30 21:12:44 -0300

    timestamps

--- makeStore.js
+++ makeStore.js
@@ -9,6 +9,7 @@ const thunk = require('redux-thunk').default
 const file = require('./lib/file')
 const Data = require('./modules/Data')
 const Tasks = require('./modules/Tasks')
+const Timestamps = require('./modules/Timestamps')
 
 const statePersister = filename => {
     const queue = new Queue(1)
@@ -20,12 +21,15 @@ const statePersister = filename => {
     }
 }
 
-module.exports = function make(initialState = {}) {
-    const persister = statePersister('state.json')
-    const middleware = applyMiddleware(thunk, persister)
+module.exports = function make(initialState = {}, options = {}) {
+    const persister = options.storage ? statePersister(options.storage) : null
+    const middleware = persister
+        ? applyMiddleware(thunk, persister)
+        : applyMiddleware(thunk)
     const reducer = combineReducers({
         data: Data.reducer,
         tasks: Tasks.reducer,
+        timestamps: Timestamps.reducer
     })
     const store = createStore(reducer, initialState, middleware)
     return store
