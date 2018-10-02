4087b97c8fe9baf50b995bad1667ffa7c841ba0a
Axel Dolce <axel.dolce@gmail.com>
2018-10-02 20:40:02 -0300

    added action/albums/mediaItems reducers

--- makeStore.js
+++ makeStore.js
@@ -7,7 +7,10 @@ const {
 const thunk = require('redux-thunk').default
 
 const file = require('./lib/file')
+
+const Albums = require('./modules/Albums')
 const Data = require('./modules/Data')
+const MediaItems = require('./modules/MediaItems')
 const Tasks = require('./modules/Tasks')
 const Timestamps = require('./modules/Timestamps')
 
@@ -27,7 +30,28 @@ module.exports = function make(initialState = {}, options = {}) {
         ? applyMiddleware(thunk, persister)
         : applyMiddleware(thunk)
     const reducer = combineReducers({
+        actions: (state = [], action) => {
+            if (action.type.indexOf('@@redux/INIT') === 0) return [action]
+            const _action = {
+                ...action,
+                $date: Date.now()
+            }
+            switch (action.type) {
+                case Albums.LOAD_ALBUMS_SUCCESS: {
+                    delete _action.payload
+                    return state.concat(_action)
+                }
+                case MediaItems.LOAD_MANY_SUCCESS: {
+                    delete _action.payload
+                    return state.concat(_action)
+                }
+                default:
+                    return state.concat(_action)
+            }
+        },
+        albums: Albums.reducer,
         data: Data.reducer,
+        mediaItems: MediaItems.reducer,
         tasks: Tasks.reducer,
         timestamps: Timestamps.reducer
     })
