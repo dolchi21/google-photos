515ede9ae64cf88f1fb4715c215ef3c49b455948
Axel Dolce <axel.dolce@gmail.com>
2018-10-02 20:36:41 -0300

    Albums module

---
+++ modules/Albums.js
@@ -0,0 +1,51 @@
+//@ts-check
+const LOAD_ALBUM = 'LOAD_ALBUM'
+const LOAD_ALBUM_SUCCESS = 'LOAD_ALBUM_SUCCESS'
+const LOAD_ALBUMS = 'LOAD_ALBUMS'
+const LOAD_ALBUMS_SUCCESS = 'LOAD_ALBUMS_SUCCESS'
+const LOAD_ALBUMS_ERROR = 'LOAD_ALBUMS_ERROR'
+
+function reducer(state = [], action) {
+    const { type, payload } = action
+    switch (type) {
+        case LOAD_ALBUM_SUCCESS: {
+            const nextState = state.filter(album => album.id !== payload.id)
+            return nextState.concat(payload)
+        }
+        case LOAD_ALBUMS_SUCCESS: {
+            return [].concat(payload)
+        }
+        default: {
+            return state
+        }
+    }
+}
+
+const album = album => ({
+    type: LOAD_ALBUM_SUCCESS,
+    payload: album
+})
+
+const loadAlbums = gPhotos => async dispatch => {
+    dispatch({ type: LOAD_ALBUMS })
+    try {
+        const albums = await gPhotos.albums()
+        dispatch(set(albums))
+        return albums
+    } catch (err) {
+        dispatch({ type: LOAD_ALBUMS_ERROR, payload: err })
+    }
+}
+
+const set = albums => ({
+    type: LOAD_ALBUMS_SUCCESS,
+    payload: albums
+})
+
+module.exports = {
+    LOAD_ALBUMS_SUCCESS,
+    reducer,
+    album,
+    loadAlbums,
+    set
+}
