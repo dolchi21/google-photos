a095fd8afba703b5d330a557cfd2665a7adf3790
Axel Dolce <axel.dolce@gmail.com>
2018-10-02 20:41:14 -0300

    entities age selectors

--- selectors.js
+++ selectors.js
@@ -1,8 +1,18 @@
-const albums = module.exports.albums = state => state.data.albums
+const Timestamps = require('./modules/Timestamps')
+
+const albums = module.exports.albums = state => state.albums
 
 const uploadsAlbum = module.exports.uploadsAlbum = state => {
     return albums(state).find(({ title }) => {
         const isWriteable = true
         return isWriteable && (title.toLowerCase() === 'uploads')
     })
+}
+
+const albumsAge = module.exports.albumsAge = state => {
+    return Timestamps.albumsAge(state.timestamps)
+}
+
+const mediaItemsAge = module.exports.mediaItemsAge = state => {
+    return Timestamps.mediaItemsAge(state.timestamps)
 }
