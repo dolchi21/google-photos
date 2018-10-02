dbeac98e8e9b7e4bb128bb66ab6920a042117893
Axel Dolce <axel.dolce@gmail.com>
2018-10-02 20:39:41 -0300

    update

--- modules/Timestamps.js
+++ modules/Timestamps.js
@@ -1,7 +1,26 @@
+//@ts-check
+const { LOAD_ALBUMS_SUCCESS } = require('./Albums')
+const MediaItems = require('./MediaItems')
 
 const reducer = module.exports.reducer = function reducer(state = {}, action) {
-    return {
-        ...state,
-        [action.type]: new Date()
+    if (action.type.indexOf('@@redux/INIT') === 0) return state
+    switch (action.type) {
+        default:
+            return {
+                ...state,
+                [action.type]: Date.now()
+            }
     }
+}
+
+const albumsAge = module.exports.albumsAge = state => {
+    const updatedAt = state[LOAD_ALBUMS_SUCCESS]
+    if (!updatedAt) return Infinity
+    return Date.now() - updatedAt
+}
+
+const mediaItemsAge = module.exports.mediaItemsAge = state => {
+    const updatedAt = state[MediaItems.LOAD_MANY_SUCCESS]
+    if (!updatedAt) return Infinity
+    return Date.now() - updatedAt
 }
