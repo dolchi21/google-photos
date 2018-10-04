50699e768298a6c69ea8a3a662e0908c1c8e5abd
Axel Dolce <axel.dolce@gmail.com>
2018-10-04 17:47:00 -0300

    import reducer

--- makeStore.js
+++ makeStore.js
@@ -1,18 +1,11 @@
 const Queue = require('promise-queue')
 const {
     applyMiddleware,
-    combineReducers,
     createStore,
 } = require('redux')
 const thunk = require('redux-thunk').default
 
 const file = require('./lib/file')
-
-const Albums = require('./modules/Albums')
-const Data = require('./modules/Data')
-const MediaItems = require('./modules/MediaItems')
-const Tasks = require('./modules/Tasks')
-const Timestamps = require('./modules/Timestamps')
 
 const statePersister = filename => {
     const queue = new Queue(1)
@@ -24,37 +17,14 @@ const statePersister = filename => {
     }
 }
 
+const reducer = require('./modules/reducer')
+
 module.exports = function make(initialState = {}, options = {}) {
     const persister = options.storage ? statePersister(options.storage) : null
     const middleware = persister
         ? applyMiddleware(thunk, persister)
         : applyMiddleware(thunk)
-    const reducer = combineReducers({
-        actions: (state = [], action) => {
-            if (action.type.indexOf('@@redux/INIT') === 0) return [action]
-            const _action = {
-                ...action,
-                $date: Date.now()
-            }
-            switch (action.type) {
-                case Albums.LOAD_ALBUMS_SUCCESS: {
-                    delete _action.payload
-                    return state.concat(_action)
-                }
-                case MediaItems.LOAD_MANY_SUCCESS: {
-                    delete _action.payload
-                    return state.concat(_action)
-                }
-                default:
-                    return state.concat(_action)
-            }
-        },
-        albums: Albums.reducer,
-        data: Data.reducer,
-        mediaItems: MediaItems.reducer,
-        tasks: Tasks.reducer,
-        timestamps: Timestamps.reducer
-    })
+    
     const store = createStore(reducer, initialState, middleware)
     return store
 }
