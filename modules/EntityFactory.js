bf1f3dce48863c30122bc4c7a80bbccb628bfb70
Axel Dolce <axel.dolce@gmail.com>
2018-10-04 17:46:32 -0300

    update

--- modules/EntityFactory.js
+++ modules/EntityFactory.js
@@ -5,19 +5,27 @@ const Task = (start, end, error) => ({
     error: payload => ({ type: error, payload })
 })
 module.exports = (namespace, options = {}) => {
+    const ADD = `${namespace}/ADD`
     const LOAD_ONE = `${namespace}/LOAD_ONE`
     const LOAD_ONE_SUCCESS = `${namespace}/LOAD_ONE_SUCCESS`
     const LOAD_MANY = `${namespace}/LOAD_MANY`
     const LOAD_MANY_SUCCESS = `${namespace}/LOAD_MANY_SUCCESS`
+    const SET = `${namespace}/SET`
 
     const reducer = (state = [], action) => {
         const { type, payload } = action
         switch (type) {
+            case ADD: {
+                const newIds = payload.map(e => e.id)
+                const nextState = state.filter(e => newIds.indexOf(e.id) === -1)
+                return nextState.concat(payload)
+            }
             case LOAD_ONE_SUCCESS: {
                 const nextState = state.filter(e => e.id === payload.id)
                 return nextState.concat(payload)
             }
-            case LOAD_MANY_SUCCESS: {
+            case LOAD_MANY_SUCCESS:
+            case SET: {
                 return [].concat(action.payload)
             }
             default: {
@@ -30,13 +38,28 @@ module.exports = (namespace, options = {}) => {
         LOAD_ONE_SUCCESS,
         LOAD_MANY_SUCCESS,
         reducer,
-        many: Task(LOAD_MANY, LOAD_MANY_SUCCESS),
+        add: payload => ({
+            type: ADD,
+            payload
+        }),
+        //many: Task(LOAD_MANY, LOAD_MANY_SUCCESS),
+        many: () => ({
+            start: () => ({ type: LOAD_MANY }),
+            end: payload => ({
+                type: LOAD_MANY_SUCCESS,
+                payload
+            })
+        }),
         one: () => ({
             start: () => ({ type: LOAD_ONE }),
             end: payload => ({
                 type: LOAD_ONE_SUCCESS,
                 payload
             })
+        }),
+        set: payload => ({
+            type: SET,
+            payload
         })
     }
 }
